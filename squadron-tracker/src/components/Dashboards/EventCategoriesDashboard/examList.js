fbaaa9cddeaa93ebc3257b28ceb2d54c0746701e
HMIL1151 <157037796+HMIL1151@users.noreply.github.com>
2025-03-24 22:48:41 +0000

    just the start of mass event log and categories work

---
+++ squadron-tracker/src/components/Dashboards/EventCategoriesDashboard/examList.js
@@ -0,0 +1,16 @@
+export const examList = [
+    "Principles of Flight",
+    "Airmanship Knowledge",
+    "Basic Navigation using a Map and Compass",
+    "Air Power",
+    "Airframes",
+    "Piston Engine Propulsion",
+    "Jet Engine Propulsion",
+    "Principles of Rocketry",
+    "Aircraft Handling and Flying Techniques",
+    "Air Navigation",
+    "Pilot Navigation",
+    "Military Aircraft Systems",
+    "Satellite and Data Communications",
+    "Radio and Radar"
+];
