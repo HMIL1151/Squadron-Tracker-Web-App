13151efefc3bab74ec384ae46ebd0de687af186a
HMIL1151 <157037796+HMIL1151@users.noreply.github.com>
2025-03-26 10:05:10 +0000

    initial interface for event log
    purely pulling from firestore and displaying. No add event funcionality yet!

--- squadron-tracker/src/components/Dashboards/Mass Event Log/MassEventLog.js
+++ squadron-tracker/src/components/Dashboards/Mass Event Log/MassEventLog.js
@@ -1,10 +1,63 @@
-const MassEventLog = () => {
-    return (
-      <div>
-        <h2>Mass Event Log</h2>
-        <p>Mass Event Log under construction 🏗️.</p>
+import React, { useState, useEffect } from "react";
+import { fetchCollectionData } from "../../../firebase/firestoreUtils";
+import Table from "../../Table/Table";
+import "./MassEventLog.css";
+
+const MassEventLog = ({ user }) => {
+  const [events, setEvents] = useState([]);
+
+  const columns = ["Name", "Event", "Date", "Points"];
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const eventData = await fetchCollectionData("Event Log");
+        const formattedEvents = eventData.map((event) => {
+          const { badgeCategory, badgeLevel, examName, eventName, specialAward } = event;
+
+          let eventDescription = "";
+          if (badgeCategory) {
+            eventDescription = `${badgeLevel} ${badgeCategory}`;
+          } else if (examName) {
+            eventDescription = `${examName} Exam`;
+          } else if (eventName) {
+            eventDescription = eventName;
+          } else if (specialAward) {
+            eventDescription = specialAward;
+          } else {
+            throw new Error("Invalid event data: Missing required fields for event description.");
+          }
+
+          return {
+            Name: event.cadetName,
+            Event: eventDescription,
+            Date: event.date,
+            Points: Math.floor(Math.random() * 10) + 1, // Random number between 1-10
+            AddedBy: event.addedBy,
+            CreatedAt: event.createdAt,
+            id: event.id, // Add the event ID
+          };
+        });
+
+        setEvents(formattedEvents);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  return (
+    <div className="table-dashboard-container">
+      <div className="button-container">
+        <button className="table-button">
+          Add New Event
+        </button>
       </div>
-    );
-  };
-  
-  export default MassEventLog;
+      <Table columns={columns} data={events} disableHover={false} width="80%" />
+    </div>
+  );
+};
+
+export default MassEventLog;
