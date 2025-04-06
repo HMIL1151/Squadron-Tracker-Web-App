dba208e32daf454cfaf012ec20603506b8e5d436
HMIL1151 <157037796+HMIL1151@users.noreply.github.com>
2025-04-06 11:13:26 +0100

    removed menu collapse

--- squadron-tracker/src/components/Menu/Menu.js
+++ squadron-tracker/src/components/Menu/Menu.js
@@ -1,36 +1,26 @@
 //TODO: Collapsing Menu
 
-import React, { useState } from "react";
+import React from "react";
 import dashboardList from "../Dashboards/Dashboard Components/dashboardList";
 import "./Menu.css";
 
 const Menu = ({ activeMenu, setActiveMenu, isAdmin }) => {
-  const [isCollapsed, setIsCollapsed] = useState(false); // State to track if the menu is collapsed
-
   return (
-    <div className={`menu-container ${isCollapsed ? "collapsed" : ""}`}>
-      <div className="menu">
-        <ul>
-          {dashboardList
-            .filter((dashboard) => (dashboard.adminOnly ? isAdmin : true)) // Filter admin-only dashboards
-            .map((dashboard) => (
-              <li
-                key={dashboard.key}
-                className={activeMenu === dashboard.key ? "active" : ""}
-                onClick={() => setActiveMenu(dashboard.key)}
-              >
-                {dashboard.title}
-              </li>
-            ))}
-        </ul>
-      </div>
-      <button
-        className="menu-toggle-tab"
-        onClick={() => setIsCollapsed((prev) => !prev)}
-      >
-        {isCollapsed ? ">" : "<"}
-      </button>
-    </div>
+    <nav className="menu">
+      <ul>
+        {dashboardList
+          .filter((dashboard) => (dashboard.adminOnly ? isAdmin : true)) // Filter admin-only dashboards
+          .map((dashboard) => (
+            <li
+              key={dashboard.key}
+              className={activeMenu === dashboard.key ? "active" : ""}
+              onClick={() => setActiveMenu(dashboard.key)}
+            >
+              {dashboard.title}
+            </li>
+          ))}
+      </ul>
+    </nav>
   );
 };
 
